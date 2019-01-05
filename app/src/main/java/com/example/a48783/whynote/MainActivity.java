f6af52c0bac550c7f0a9f16831622d4aef380ca7
Anka <annazurczak1@gmail.com>
2019-01-05 18:05:06 +0100

    add login panel

--- app/src/main/java/com/example/a48783/whynote/MainActivity.java
+++ app/src/main/java/com/example/a48783/whynote/MainActivity.java
@@ -3,11 +3,21 @@ package com.example.a48783.whynote;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void logIn(View view)
+    {
+       // Intent intent = new Intent(this, Menu.class);
+       // startActivity(intent);
     }
 }
