8064368fbe6036dd545769914b87533ff8da71c0
Anka <annazurczak1@gmail.com>
2019-01-06 13:26:36 +0100

    add new subject

--- app/src/main/java/com/example/a48783/whynote/Subjects.java
+++ app/src/main/java/com/example/a48783/whynote/Subjects.java
@@ -1,11 +1,15 @@
 package com.example.a48783.whynote;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.ListView;
 
 import java.util.ArrayList;
@@ -44,6 +48,37 @@ public class Subjects extends AppCompatActivity {
         Intent intent = new Intent(this, LastNotes.class);
         intent.putExtra("subject",subjects.get(i) );
         startActivity(intent);
+    }
+
+    public void addNew(View view){
+        AlertDialog.Builder alertDialog = new AlertDialog.Builder(Subjects.this);
+        alertDialog.setTitle("Add new subject");
+        alertDialog.setMessage("Enter subject name");
+        final EditText input = new EditText(Subjects.this);
+        LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.MATCH_PARENT);
+        input.setLayoutParams(lp);
+        alertDialog.setView(input);
+
+        alertDialog.setPositiveButton("ADD",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                       String new_subject = input.getText().toString();
+                                subjects.add(new_subject);
+                                //adapter.add(new_subject);
+                                adapter.notifyDataSetChanged();
+                    }
+                });
+
+        alertDialog.setNegativeButton("CANCEL",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+                    }
+                });
+
+        alertDialog.show();
     }
 
 }
